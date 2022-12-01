bf57535f8ccc58eabb4ea92c2c20d58838c9975e
sevys <201228@ids.upchiapas.edu.mx>
2022-12-01 03:39:38 -0600

    estructura

---
+++ lib/screen/mi_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class MiHomePage extends StatelessWidget {
+  const MiHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hola'),
+      ),
+    );
+  }
+}
